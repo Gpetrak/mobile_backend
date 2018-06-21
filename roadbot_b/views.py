3595145a13796dccd955b1de44afdf1de997550f
gpetrak <gkpetrak@gmail.com>
2018-06-21 14:11:00 +0300

    querying the db

--- roadbot_b/views.py
+++ roadbot_b/views.py
@@ -4,33 +4,29 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.contrib.gis.geos import Point
 from roadbot_b.models import Accidents
-from django.views.decorators.csrf import csrf_exempt
+# from django.views.decorators.csrf import csrf_exempt
 import json
 
-@csrf_exempt
 def accident_info(request):
     if request.method == 'GET':
-        # convert json data to python dictionary
-        # latlng = json.loads(request.body)
-        # latitude = latlng['lat']
-        # longitude = latlng['lng']
-        # lat = request.POST.get('latitude')
-        # lng = request.POST.get('longitude')
 
-        # lat = float(lat)
-        # lng = float(lng)
+        lat = request.GET.get('lat')
+        lng = request.GET.get('lng')
 
-        # location = Point(lng, lat, srid=4326)
+        lat = float(lat)
+        lng = float(lng)
+  
+        location = Point(lng, lat, srid=4326)
 
-        # danger_zone = Accidents.objects.filter(geom__contains=location)
+        danger_zone = Accidents.objects.filter(geom__contains=location)
 
-        #if danger_zone:
-        #    result = "Attention: Danger Zone %s" % danger_zone
-        #else:
-        #    result = "keep walking"
-        
-        return HttpResponse("success")
-                            #content_type = 'application/json'
+        if danger_zone:
+            result = "Attention: Danger Zone %s" % danger_zone
+        else:
+            result = "keep walking"
+        res_json = json.dumps(result)
+        return HttpResponse(res_json,
+                            content_type = 'application/json')
          
 
         
